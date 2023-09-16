82d68125555d670d29810768b84daf31e37adb53
guyddogl <guyddogl@gmail.com>
2023-09-16 18:45:15 -0300

    Items schemas

---
+++ src/schemas/index.ts
@@ -0,0 +1,7 @@
+import { createItemSchema, getItemSchema, updateItemSchema } from './itemsSchemas';
+
+export {
+	createItemSchema,
+	getItemSchema,
+	updateItemSchema,
+};
---
+++ src/schemas/itemsSchemas.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+const createItemSchema = z.object({
+	name: z.string().min(1),
+});
+
+const getItemSchema = z.object({
+	id: z.string().min(1),
+});
+
+const updateItemSchema = z.object({
+	id: z.number().min(1),
+	name: z.string().min(1),
+	checked: z.boolean().optional(),
+});
+
+export {
+	createItemSchema,
+	getItemSchema,
+	updateItemSchema,
+};
