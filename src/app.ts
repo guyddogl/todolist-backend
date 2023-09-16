50caeef14931f1f9ec166d704c73411d26be8068
guyddogl <guyddogl@gmail.com>
2023-09-16 18:45:42 -0300

    itemsRoutes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-import { status } from './routes/index';
+import { itemsRoutes, status } from './routes/index';
 
-const baseApiUrl = '/api';
-
-// App Express
 const app = express();
 
-// Cors
 app.use(cors({
 	origin: ['http://localhost:3000'],
 }));
@@ -17,12 +13,11 @@ app.use(express.json());
 app.get('/', async (req, res) => {
 	res.send('Hello, world!');
 });
-app.get(`${baseApiUrl}/`, async (req, res) => {
-	res.send('API status OK');
-});
-app.use(`${baseApiUrl}/status`, status);
 
-// Resposta padrão para quaisquer outras requisições:
+app.use('/items', itemsRoutes);
+
+app.use('/status', status);
+
 app.use((req, res) => {
 	res.status(404);
 });
