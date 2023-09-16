6da8a8be1b25455794a179c7da264ad32b84fb2e
guyddogl <guyddogl@gmail.com>
2023-09-16 18:45:28 -0300

    Remove comments

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,8 @@ import app from './app';
 
 dotenv.config();
 
-// Porta do servidor
 const PORT = process.env.PORT || 4000;
 
-// Inicia o servidor
 app.listen(PORT, () => {
 	console.log(`Server listening on port: ${PORT}`);
 });
