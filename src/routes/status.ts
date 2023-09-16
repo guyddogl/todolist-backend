739729f84bab1559d7a9f0381e992d5460c5246f
guyddogl <guyddogl@gmail.com>
2023-09-16 18:46:03 -0300

    Server status feedback

--- src/routes/status.ts
+++ src/routes/status.ts
@@ -1,18 +1,23 @@
-import connection from '../database/connection';
 import express from 'express';
+import prisma from '../database/prisma';
 const route = express();
 
 route.get('/', async (req, res) => {
-	res.send('Backend status OK');
-});
+	const response = {
+		API: 'status ok',
+		'DB': '',
+	};
 
-route.get('/db', async (req, res) => {
 	try {
-		await connection.query('SELECT 1');
-		res.send('Conexão com o banco de dados está funcionando!');
-	} catch (err) {
-		res.status(500).send('Erro ao conectar ao banco de dados');
+		await prisma.$connect();
+		response['DB'] = 'Conexão com o banco de dados estabelecida com sucesso.';
+	} catch (error) {
+		response['DB'] = 'Erro ao conectar ao banco de dados.';
+	} finally {
+		await prisma.$disconnect();
 	}
+
+	res.json(response);
 });
 
 export default route;
